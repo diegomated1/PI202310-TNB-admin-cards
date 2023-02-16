2781320b8ad6ebb58bfa8b24bcaab9144135af64
diegomated1 <diegocardwr@gmail.com>
2023-02-16 14:34:40 -0500

    new server structure

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
-// IMPORTS
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
-const app = express();
+//import userR from './router/user.route.js';
+import cardsRouter from './router/cards.router.js';
 
-// CONFIG
-dotenv.config();
+class Server{
 
-// MIDDLEWARES
-app.use(express.json());
-app.use(morgan('dev'));
+    private app: express.Express
+    
+    constructor(){
+        this.app = express();
+        this.config();
+        this.routes();
+        this.start();
+    }
 
-// ROUTER
+    private config(){
+        dotenv.config();
+        this.app.use(express.json());
+        this.app.use(morgan('dev'));
+    }
 
-// LISTEN
-app.listen(parseInt(process.env.API_PORT!), process.env.API_HOST!, ()=>{
-    console.log(`Listen on http://${process.env.API_HOST}:${process.env.API_PORT}/`);
-});
+    private routes(){
+        this.app.use(cardsRouter.router);
+    }
+
+    private start(){
+        this.app.listen(parseInt(process.env.API_PORT!), process.env.API_HOST!, ()=>{
+            console.log(`Listen on http://${process.env.API_HOST}:${process.env.API_PORT}/`);
+        });
+    }
+}
+
+new Server();
