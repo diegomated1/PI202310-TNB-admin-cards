6cececc665c8bc37d79a54acc9f1ac26aadf4d95
unknown <kevin.quintana.256bit@gmail.com>
2023-02-17 15:12:23 -0500

    entidad version 2

--- src/models/cards.model.ts
+++ src/models/cards.model.ts
@@ -4,7 +4,11 @@ import db from '../database/mongo.db.js';
 const CardModel = db.model('cards', new mongoose.Schema({
     _id: String,
     name: String,
-    description: String
+    description: String,
+    id_hero: String,
+    image: String,
+    card_type: Number,
+    Efects: Array,
 }));
 
 export default CardModel;
