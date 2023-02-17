c00d027871b82b62ea9253534762f593c8713fa8
diegomated1 <diegocardwr@gmail.com>
2023-02-17 15:20:46 -0500

    card model

--- src/models/cards.model.ts
+++ src/models/cards.model.ts
@@ -4,7 +4,8 @@ import db from '../database/mongo.db.js';
 const CardModel = db.model('cards', new mongoose.Schema({
     _id: String,
     name: String,
-    description: String
+    description: String,
+    type: Number
 }));
 
 export default CardModel;
