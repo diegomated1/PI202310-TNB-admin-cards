9d3fe964a1f2f9b3d3f8cc623e7b30101025f12f
unknown <kevin.quintana.256bit@gmail.com>
2023-02-25 14:57:06 -0500

    devolver errores

--- src/controllers/heroes.controllers.ts
+++ src/controllers/heroes.controllers.ts
@@ -7,10 +7,10 @@ const insertHeroes = async (req:Request, res:Response, next: NextFunction)=>{
       const card_info = req.body;
       const card = new CardModel({...card_info, _id: ui.process()});
       await card.save();
-      res.status(200).json({status: true});
+      res.status(200).json({error:0,status: true, message:'creado correctamente'});
   }catch(error){
       console.log(error)
-      res.status(500).json({status: false});
+      res.status(500).json({error:1,status: false, message:'no se pudo crear'});
   }
 }
 
